9a678aee5ceb1c3201adf78b8c7302bdaa83c12a
Michael Loschiavo <losch103@gmail.com>
2016-07-15 11:22:25 -0700

    Completed functional timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,13 +1,65 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
 
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    }
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({
+            count: 0,
+            countdownStatus: 'paused'
+          });
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+
+
+  startTimer: function () {
+    this.timer = setInterval(()=> {
+      var newCount = this.state.count + 1
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'})
+      }
+    }, 1000)
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    })
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({
+      countdownStatus: newStatus
+    })
+  },
   render: function() {
+    var {count, countdownStatus} = this.state
+
     return (
       <div>
-        <p>
-          Timer component rendered
-        </p>
+        <h1 className="page-title">Timer</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
       </div>
     );
   }
